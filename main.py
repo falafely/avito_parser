e0ae596a74f8a589575e5ca333058d53958bc394
falafely <126647920+falafely@users.noreply.github.com>
2023-07-14 19:45:52 +0300

    Update main.py
    replaced "laptop/s" with "product/s"

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ def main():
     import requests
     from bs4 import BeautifulSoup as bs
 
-    user_choose = input('Что ищем?'
-                        '\n')
+    user_choose = input('Что ищем?\n')
 
     url = ('https://www.avito.ru/krasnodarskiy_kray?q=' + user_choose)
     r = requests.get(url)
     soap = bs(r.text, 'html.parser')
 
-    laptops = soap.find_all(class_='iva-item-title-py3i_')
+    products = soap.find_all(class_='iva-item-title-py3i_')
 
-    for laptop in laptops:
-        name = laptop.text
-        link = laptop.find('a')['href']
+    for product in products:
+        name = product.text
+        link = product.find('a')['href']
         print(f'{name} | https://www.avito.ru{link}')
 
 if __name__ == '__main__':
